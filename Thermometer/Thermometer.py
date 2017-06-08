d9657d050b089c3b8c465454490e7cf432776132
Andrew Schofield <the.uncle.fungus@gmail.com>
2017-06-08 22:32:51 +0100

    Add offset and option to switch between C and F

--- Thermometer/Thermometer.py
+++ Thermometer/Thermometer.py
@@ -1,8 +1,16 @@
 # Add your Python code here. E.g.
 from microbit import *
 
+unit = "C"
+offset = -2
 
 while True:
-    temp = temperature()
-    display.scroll(str(temp) + "C", delay=250)
-    sleep(2000)
+    if button_a.is_pressed():
+        unit = "C"
+    elif button_b.is_pressed():
+        unit = "F"
+    temp = temperature() + offset
+    if unit == "F":
+        temp = int(temp * 9/5 + 32)
+    display.scroll(str(temp) + unit, delay=200, wait=False)
+    sleep(5000)
